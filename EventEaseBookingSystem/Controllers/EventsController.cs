e323f9fbef8e8ac427dc3c7ec2a92b41d119aedd
ST10446744 <165274678+ST10446744@users.noreply.github.com>
2026-03-13 13:42:39 +0200

    EventsController.cs

--- EventEaseBookingSystem/Controllers/EventsController.cs
+++ EventEaseBookingSystem/Controllers/EventsController.cs
@@ -24,7 +24,7 @@ namespace EventEaseBookingSystem.Controllers
             return View(await _context.Events.ToListAsync());
         }
 
-        // GET: Events/Details/5
+        
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -42,15 +42,13 @@ namespace EventEaseBookingSystem.Controllers
             return View(@event);
         }
 
-        // GET: Events/Create
+        
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Events/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+     
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("EventID,EventName,StartDate,EndDate,ImageUrl")] Event @event)
@@ -64,7 +62,7 @@ namespace EventEaseBookingSystem.Controllers
             return View(@event);
         }
 
-        // GET: Events/Edit/5
+        
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -80,9 +78,7 @@ namespace EventEaseBookingSystem.Controllers
             return View(@event);
         }
 
-        // POST: Events/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("EventID,EventName,StartDate,EndDate,ImageUrl")] Event @event)
@@ -115,7 +111,7 @@ namespace EventEaseBookingSystem.Controllers
             return View(@event);
         }
 
-        // GET: Events/Delete/5
+        
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -133,7 +129,7 @@ namespace EventEaseBookingSystem.Controllers
             return View(@event);
         }
 
-        // POST: Events/Delete/5
+        
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
